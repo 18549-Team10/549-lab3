ab3148d4f93ee0fe87b2960a8179888dbf7adb46
Selin Sirinterlikci <ssirinte@andrew.cmu.edu>
2017-03-05 15:54:27 -0500

    Added UART confirmation for each mode when selected

--- uart/uart_test.c
+++ uart/uart_test.c
@@ -67,6 +67,7 @@ int main(void)
    while(1) {
       ch = getchar();
       if (mode == SENSOR_MODE) {
+         putchar('s');
          /*float temp_val = getTemp();
          int float_size = 48;
          char[float_size] temp_val_string;
@@ -88,7 +89,7 @@ int main(void)
          PORTD = color;
 
       } else {
-         
+         putchar('b');
       }
 
       /* A bunch of if...else if... gives smaller code than switch...case ! */
